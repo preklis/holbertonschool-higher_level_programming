e6e7164cc4aceedbe48ed481ba2d9c13b1998f12
Nizami <preklis@protonmail.com>
2026-04-04 04:54:50 +0400

    100

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,27 +1,42 @@
 #!/usr/bin/python3
-import sys
+from sys import argv
 from calculator_1 import add, sub, mul, div
 
+if __name__ == '__main__':
+    """
+    Handles basic operations
 
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+    Performs basic operations like addition, substraction,
+    multiplication and division between two numbers.
 
-    a = int(sys.argv[1])
-    op = sys.argv[2]
-    b = int(sys.argv[3])
+    The program will execute an operation between two numbers
+    selected by the operator sent to the program.
+    """
+    l_av = len(argv) - 1
 
-    if op == "+":
-        result = add(a, b)
-    elif op == "-":
-        result = sub(a, b)
-    elif op == "*":
-        result = mul(a, b)
-    elif op == "/":
-        result = div(a, b)
+    if l_av == 3:
+        operator = argv[2]
+        num_a = int(argv[1])
+        num_b = int(argv[3])
+        if operator == '+':
+            res = add(num_a, num_b)
+            print('{:d} + {:d} = {:d}'.format(num_a, num_b, res))
+            exit(0)
+        elif operator == '-':
+            res = sub(num_a, num_b)
+            print('{:d} - {:d} = {:d}'.format(num_a, num_b, res))
+            exit(0)
+        elif operator == '*':
+            res = mul(num_a, num_b)
+            print('{:d} * {:d} = {:d}'.format(num_a, num_b, res))
+            exit(0)
+        elif operator == '/':
+            res = div(num_a, num_b)
+            print('{:d} / {:d} = {:d}'.format(num_a, num_b, res))
+            exit(0)
+        else:
+            print('Unknown operator. Available operators: +, -, * and /')
+            exit(1)
     else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-
-    print("{} {} {} = {}".format(a, op, b, result))
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        exit(1)
