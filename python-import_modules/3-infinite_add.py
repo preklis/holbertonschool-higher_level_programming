a430af2a7fb1c600bdd2d1341642079844f618a3
Nizami <preklis@protonmail.com>
2026-04-04 04:15:41 +0400

    3

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    argv = sys.argv
+    argc = len(argv) 
+    ans = 0
+    for i in range(1, argc):
+        ans+=int(argv[i])
+    print(ans)
