9898d0a25f6e602927118c9db0f3ae704266820b
Nizami <preklis@protonmail.com>
2026-04-06 23:25:45 +0000

    4

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list=[], idx=0, new_element=0):
-    if idx < 0:
+    if idx < 0 or idx >= len(my_list):
         return my_list
     new_list=[]
     for i in my_list:
