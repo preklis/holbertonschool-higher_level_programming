365d082595bc4e4aa6ce59cec2cf02818128e3ec
Nizami <preklis@protonmail.com>
2026-04-06 22:59:24 +0000

    1

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-def element_at(my_list=[], idx=0):
-    if len(my_list)<idx:
+def element_at(my_list, idx):
+    if idx < 0 or idx >= len(my_list):
         return None
-    else:
-        return(my_list[idx])
+    return my_list[idx]
