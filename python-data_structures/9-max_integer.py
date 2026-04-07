be0817631259ca432d6daf64ce5c009e86fe6ebc
Nizami <preklis@protonmail.com>
2026-04-07 00:28:23 +0000

    9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def max_integer(my_list = []):
+def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
     maxz = - 2 ** 31 - 1
     for i in my_list:
         if i > maxz:
-            maxz=i
+            maxz = i
     return maxz
