b6bb763c7b48848b2bb56dcee3dc9b0d36b96a64
Nizami <preklis@protonmail.com>
2026-04-11 19:44:46 +0400

    1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(list=[], val1=0, val2=0):
     listnew = []
-    for i in range(0,len(list)):
+    for i in range(0, len(list)):
         if list[i]==val1:
             listnew.append(val2)
         else:
