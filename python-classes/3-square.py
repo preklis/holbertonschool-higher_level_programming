bc66dd011f67f28cff6000f32eca15c9f6066ec0
Nizami <preklis@protonmail.com>
2026-04-11 23:02:51 +0400

    3

---
+++ python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module that defines a Square class"""
+
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size=0):
+        """Initialize a square with size
+
+        Args:
+            size (int): size of the square
+
+        Raises:
+            TypeError: if size is not an integer
+            ValueError: if size is less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
