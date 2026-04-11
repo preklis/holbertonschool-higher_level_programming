e68e528fbadf762e2e62db61179e46a65b4eab5d
Nizami <preklis@protonmail.com>
2026-04-11 19:49:26 +0400

    0

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(list=[]):
-    checker=[]
-    sum=0
+    checker = []
+    sum = 0
     for i in list:
         if i not in checker:
-            sum+=i
+            sum += i
             checker.append(i)
     return sum
