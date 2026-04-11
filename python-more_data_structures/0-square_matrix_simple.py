a53ef03bece9d2a865ea901efb17e77d2230ca09
Nizami <preklis@protonmail.com>
2026-04-11 19:36:40 +0400

    0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[[]]):
-    new_matrix=[]
+    new_matrix = []
     for i in matrix:
-        new_i=[]
+        new_i = []
         for j in i:
             new_i.append(j**2)
         new_matrix.append(new_i)
