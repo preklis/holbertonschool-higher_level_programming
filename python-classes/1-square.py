bcfe1d77a2f70108ad7c4cc91367b61afe388030
Nizami <preklis@protonmail.com>
2026-04-11 22:54:08 +0400

    1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -7,7 +7,7 @@ This is an example for creating a class.
 class Square:
     """
     Square class represents a square.
-    Now this class is empty.
+    Now this class is doing something but I don't have enough "axod" to explain it
     """
     def __init__(self,size):
         self.__size = size
