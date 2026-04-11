876eb408c0970f6f03eb1a8fb2717fa1d985529d
Nizami <preklis@protonmail.com>
2026-04-11 20:13:58 +0400

    0

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,4 +3,4 @@ def print_sorted_dictionary(dict={}):
     lis=list(dict)
     lis.sort()
     for i in lis:
-        print("{0}: {1}".format(i,dict.get(i)))
+        print("{0}: {1}".format(i, dict.get(i)))
