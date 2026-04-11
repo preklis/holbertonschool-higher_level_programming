f8c153ab43a2acc1f42fdeaa7b831c674bf85d0c
Nizami <preklis@protonmail.com>
2026-04-11 22:48:34 +0400

    1

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This is an example for creating a class.
+"""
+
+
+class Square:
+    """
+    Square class represents a square.
+    Now this class is empty.
+    """
+    def __init__(self,size):
+        self.__size = size
