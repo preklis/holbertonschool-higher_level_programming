b128ebd1fa03b1502a4ea085cea784e667adef8c
Nizami <preklis@protonmail.com>
2026-04-11 23:11:31 +0400

    5

---
+++ python-classes/5-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Module that defines a Square class"""
+
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size=0):
+        """Initialize a square with a given size"""
+        self.size = size  # Use the setter for validation
+
+    @property
+    def size(self):
+        """Retrieve the size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square with validation"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def my_print(self):
+        """
+        Prints in stdout the square with the character #
+        """
+        print(('#' * self.__size + '\n') * self.__size, end='')
