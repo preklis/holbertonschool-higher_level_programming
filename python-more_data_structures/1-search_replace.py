41414196238dce83dbba9543ea01cac305c713fe
Nizami <preklis@protonmail.com>
2026-04-11 19:45:13 +0400

    0

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,7 +2,7 @@
 def search_replace(list=[], val1=0, val2=0):
     listnew = []
     for i in range(0, len(list)):
-        if list[i]==val1:
+        if list[i] == val1:
             listnew.append(val2)
         else:
             listnew.append(list[i])
