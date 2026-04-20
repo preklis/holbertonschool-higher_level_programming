44e84fa3abbc2f0a8f76f6a2b72dca5080776e94
Nizami <preklis@protonmail.com>
2026-04-20 14:07:49 +0400

    0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], index=0):
     count = 0
-    for id in  range (0, index):
+    for id in range(0, index):
         try:
             print(my_list[id], end="")
             count += 1
