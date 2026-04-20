2de3df7d66fe2a523ba67a7b9ec9eca50dab33b7
Nizami <preklis@protonmail.com>
2026-04-20 16:14:30 +0400

    all

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a request to a URL and displays the size of the response body in bytes
-curl -s "$1" | wc -c
+# This program takes in a URL, sends a request to that URL, and displays the size of the body of the response.
+curl -sI "$1" | grep "Content-Length:" | cut -d' ' -f2
--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Displays the body of a 200 status code response from a URL
-curl -sL "$1" 
+# This program takes in a URL, sends a GET request to the URL, and displays the body of the response.
+curl -sL "$1" -X GET -D ./header -o ./output; if grep -q "200 OK" ./header; then cat ./output; fi
