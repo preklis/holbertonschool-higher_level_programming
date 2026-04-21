5f82f790eb97d3ebe9623b5b8f875c9331ccf4f2
Nizami <preklis@protonmail.com>
2026-04-22 00:50:35 +0400

    0

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
 
     r = requests.get(url)
-    if(r.status_code == 200):
+    if r.status_code == 200 :
         print(r.text)
     else:
         print("Error code: {}".format(r.status_code))
