ec1cb798c733c91f7aef41ed9c8d5cb7cb1f311e
Nizami <preklis@protonmail.com>
2026-04-21 15:34:55 +0400

    0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -7,4 +7,4 @@ with urllib.request.urlopen(url) as response:
 print("Body response:")
 print('\t- type: {}'.format(type(body)))
 print('\t- content: {}'.format(body))
-print('\t- utf8: {}'.format(body.decode('utf-8')))
+print('\t- utf8 content: {}'.format(body.decode('utf-8')))
