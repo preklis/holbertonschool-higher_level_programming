94785e0e6c519764c255dfe45baa3dba84770885
Nizami <preklis@protonmail.com>
2026-04-21 16:29:15 +0400

    5

--- python-network_1/5-hbtn_header.py
+++ python-network_1/5-hbtn_header.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""Sends a POST request with an email and displays the response body."""
+"""Takes a URL, sends a request, and displays X-Request-Id header."""
 
 import requests
 import sys
-url = sys.argv[1]
-r = requests.get(url)
-print(r.headers.get('X-Request-Id'))
 
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    response = requests.get(url)
+    print(response.headers.get("X-Request-Id"))
