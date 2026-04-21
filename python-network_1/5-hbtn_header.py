59a1df24ff38e7e2d6095968262753072203d895
Nizami <preklis@protonmail.com>
2026-04-21 16:24:05 +0400

    5

--- python-network_1/5-hbtn_header.py
+++ python-network_1/5-hbtn_header.py
@@ -2,7 +2,8 @@
 """Sends a POST request with an email and displays the response body."""
 
 import requests
-
-r = requests.get('https://intranet.hbtn.io/status')
+import sys
+url = sys.argv[1]
+r = requests.get(url)
 print(r.headers.get('X-Request-Id'))
 
