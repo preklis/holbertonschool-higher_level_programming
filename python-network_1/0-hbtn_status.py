52afab158392c30bec4339acd4da8b7c14a50ea4
Nizami <preklis@protonmail.com>
2026-04-21 15:32:56 +0400

    0

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import urllib.request
+url = urllib.request.Request('https://intranet.hbtn.io/status')
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+
+print("Body response:")
+print('\t- type: {}'.format(type(body)))
+print('\t- content: {}'.format(body))
+print('\t- utf8: {}'.format(body.decode('utf-8')))
