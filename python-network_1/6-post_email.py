befb9ce4e51520e9aa95c6b41e61a8a1438a048a
Nizami <preklis@protonmail.com>
2026-04-22 00:35:25 +0400

    0

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
 
-    r = requests.post(url, data = {"email": email})
+    r = requests.post(url, data={"email": email})
     print("Email: {}".format(email))
