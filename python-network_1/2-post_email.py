2d041a1c3a648e092d3a9dbea9ae030d0ae9f684
Nizami <preklis@protonmail.com>
2026-04-21 15:50:33 +0400

    2

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
 
-    data = urllib.parse.urlencode({"email":email}).encode('utf-8')
-    
+    data = urllib.parse.urlencode({"email": email}).encode('utf-8')
+
     with urllib.request.urlopen(url, data) as response:
         body = response.read().decode('utf-8')
         print(body)
