1266a4d4995b38e6a89c3beab88b4fdf4251ad74
Nizami <preklis@protonmail.com>
2026-04-21 16:19:32 +0400

    4

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -3,7 +3,7 @@
 
 import requests
 
-r=requests.get('https://intranet.hbtn.io/status')
+r = requests.get('https://intranet.hbtn.io/status')
 print("Body response:")
 print("\t- type: {}".format(type(r.text)))
 print("\t- content: {}".format(r.text))
