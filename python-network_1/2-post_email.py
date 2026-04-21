71dba7771e378fdd418fad977fe0cc8fce476463
Nizami <preklis@protonmail.com>
2026-04-21 16:02:42 +0400

    3

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Sends a POST request with an email and displays the response body."""
+
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    req = urllib.request.Request(url)
+
+    try:
+        with urllib.request.urlopen(req) as resp:
+             body = resp.read()
+
+
+    except urllib.error.HTTPError as e:
+            print("Error code: {}".format(e.code))
+    else: print(body.decode('utf-8'))
