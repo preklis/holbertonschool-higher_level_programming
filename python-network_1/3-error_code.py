faee3d4b1f781460ce0d0abbea5c54c24a87a767
Nizami <preklis@protonmail.com>
2026-04-21 16:11:32 +0400

    2

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -16,4 +16,4 @@ if __name__ == "__main__":
             print(body)
 
     except urllib.error.HTTPError as e:
-            print("Error code: {}".format(e.code))
+        print("Error code: {}".format(e.code))
