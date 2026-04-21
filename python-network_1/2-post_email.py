d743044986166b69f66ad55a089e4517f91beeda
Nizami <preklis@protonmail.com>
2026-04-21 16:09:30 +0400

    3

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
 
     try:
         with urllib.request.urlopen(req) as resp:
-            body = resp.read().deocde('utf-8')
+            body = resp.read().decode('utf-8')
             print(body)
 
     except urllib.error.HTTPError as e:
